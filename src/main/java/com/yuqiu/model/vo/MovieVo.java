0bed48a5e183771318ec969379d4b46976a0a3de
yuqiu2004 <2506852507@qq.com>
2025-06-01 21:57:57 +0800

    fix vo field type

--- src/main/java/com/yuqiu/model/vo/MovieVo.java
+++ src/main/java/com/yuqiu/model/vo/MovieVo.java
@@ -18,5 +18,5 @@ public class MovieVo {
     private String cover;
     private String mainType;  // 1=电影，2=番剧
     private Integer type;  // 1=电影，2=番剧
-    private List<String> genres;
+    private String genres;
 }
