120fdec2124c1e0b10ed232c3a2329d69027612e
yuqiu2004 <2506852507@qq.com>
2025-06-04 23:03:32 +0800

    fix build file

--- irec-front/next.config.mjs
+++ irec-front/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
-        const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+        // const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+        const apiUrl = 'http://113.47.14.108:8080/';
         return [
             {
                 source: '/api/:path*',
